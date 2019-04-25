fa4d40ac4fca915f871773f81130f5975b3e460c
Chunying Yen <chunyingyen.tw@gmail.com>
2019-04-25 15:08:15 +0800

    init

---
+++ HENCODER-02/app/src/main/java/com/hencoder/hencoderpracticedraw2/practice/Practice12PathEffectView.java
@@ -0,0 +1,103 @@
+package com.hencoder.hencoderpracticedraw2.practice;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.ComposePathEffect;
+import android.graphics.CornerPathEffect;
+import android.graphics.DashPathEffect;
+import android.graphics.DiscretePathEffect;
+import android.graphics.Paint;
+import android.graphics.Path;
+import android.graphics.PathDashPathEffect;
+import android.graphics.PathEffect;
+import android.graphics.SumPathEffect;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+import android.view.View;
+
+public class Practice12PathEffectView extends View {
+
+    Paint paint = new Paint(Paint.ANTI_ALIAS_FLAG);
+    Path path = new Path();
+
+    private PathEffect corner = new CornerPathEffect(20);
+    private PathEffect discrete = new DiscretePathEffect(20f, 5f);
+    private PathEffect dash = new DashPathEffect(new float[]{20, 10}, 0);
+    private PathEffect sum = new SumPathEffect(dash, discrete);
+    private PathEffect compose = new ComposePathEffect(dash, discrete);
+    private PathEffect pathDash;
+
+    public Practice12PathEffectView(Context context) {
+        super(context);
+    }
+
+    public Practice12PathEffectView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public Practice12PathEffectView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    {
+        paint.setStyle(Paint.Style.STROKE);
+
+        path.moveTo(50, 100);
+        path.rLineTo(50, 100);
+        path.rLineTo(80, -150);
+        path.rLineTo(100, 100);
+        path.rLineTo(70, -120);
+        path.rLineTo(150, 80);
+
+        Path p = new Path();
+        p.lineTo(20, 40);
+        p.lineTo(40, 0);
+        pathDash = new PathDashPathEffect(p, 60, 0, PathDashPathEffect.Style.TRANSLATE);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+
+        // 使用 Paint.setPathEffect() 来设置不同的 PathEffect
+
+        // 第一处：CornerPathEffect
+        paint.setPathEffect(corner);
+        canvas.drawPath(path, paint);
+
+        canvas.save();
+        canvas.translate(500, 0);
+        // 第二处：DiscretePathEffect
+        paint.setPathEffect(discrete);
+        canvas.drawPath(path, paint);
+        canvas.restore();
+
+        canvas.save();
+        canvas.translate(0, 200);
+        // 第三处：DashPathEffect
+        paint.setPathEffect(dash);
+        canvas.drawPath(path, paint);
+        canvas.restore();
+
+        canvas.save();
+        canvas.translate(500, 200);
+        // 第四处：PathDashPathEffect
+        paint.setPathEffect(pathDash);
+        canvas.drawPath(path, paint);
+        canvas.restore();
+
+        canvas.save();
+        canvas.translate(0, 400);
+        // 第五处：SumPathEffect
+        paint.setPathEffect(sum);
+        canvas.drawPath(path, paint);
+        canvas.restore();
+
+        canvas.save();
+        canvas.translate(500, 400);
+        // 第六处：ComposePathEffect
+        paint.setPathEffect(compose);
+        canvas.drawPath(path, paint);
+        canvas.restore();
+    }
+}
---
+++ HENCODER-02/app/src/main/java/com/hencoder/hencoderpracticedraw2/practice/Practice14MaskFilterView.java
@@ -0,0 +1,65 @@
+package com.hencoder.hencoderpracticedraw2.practice;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.BlurMaskFilter;
+import android.graphics.Canvas;
+import android.graphics.MaskFilter;
+import android.graphics.Paint;
+import android.graphics.Path;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+import android.view.View;
+
+import com.hencoder.hencoderpracticedraw2.R;
+
+public class Practice14MaskFilterView extends View {
+    Paint paint = new Paint(Paint.ANTI_ALIAS_FLAG);
+    Bitmap bitmap;
+
+    public Practice14MaskFilterView(Context context) {
+        super(context);
+    }
+
+    public Practice14MaskFilterView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public Practice14MaskFilterView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    {
+        setLayerType(LAYER_TYPE_SOFTWARE, null);
+        bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.what_the_fuck);
+    }
+
+    private MaskFilter blurNormal = new BlurMaskFilter(50, BlurMaskFilter.Blur.NORMAL);
+    private MaskFilter blurInner = new BlurMaskFilter(50, BlurMaskFilter.Blur.INNER);
+    private MaskFilter blurOuter = new BlurMaskFilter(50, BlurMaskFilter.Blur.OUTER);
+    private MaskFilter blurSolid = new BlurMaskFilter(50, BlurMaskFilter.Blur.SOLID);
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+
+        // 用 Paint.setMaskFilter 来设置不同的 BlurMaskFilter
+
+        // 第一个：NORMAL
+        paint.setMaskFilter(blurNormal);
+        canvas.drawBitmap(bitmap, 100, 50, paint);
+
+        // 第二个：INNER
+        paint.setMaskFilter(blurInner);
+        canvas.drawBitmap(bitmap, bitmap.getWidth() + 200, 50, paint);
+
+        // 第三个：OUTER
+        paint.setMaskFilter(blurOuter);
+        canvas.drawBitmap(bitmap, 100, bitmap.getHeight() + 100, paint);
+
+        // 第四个：SOLID
+        paint.setMaskFilter(blurSolid);
+        canvas.drawBitmap(bitmap, bitmap.getWidth() + 200, bitmap.getHeight() + 100, paint);
+    }
+}
